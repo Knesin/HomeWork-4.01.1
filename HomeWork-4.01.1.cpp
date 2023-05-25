efb0c23021b3cab3c7a46212ea4478e5162fc81b
Maksim Kl <GuruHacker@yandex.ru>
2023-05-25 21:26:51 +0300

    Initial commit

---
+++ HomeWork-4.01.1.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main()
+{
+    std::vector<int> arr = { 4, 7, 9, 14, 12 };
+
+    setlocale(LC_ALL, "Russian");
+    std::cout << "Входные данные:  ";
+    std::for_each(arr.begin(), arr.end(), [](int i) {
+        std::cout << i << " ";
+        });
+    std::cout << std::endl;
+    std::cout << "Выходные данные: ";
+    std::for_each(arr.begin(), arr.end(), [](int& i) {
+        if (i % 2) i *= 3;
+        std::cout << i << " ";
+        });
+    std::cout << std::endl;
+}
